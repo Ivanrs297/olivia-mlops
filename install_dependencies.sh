762c60bbdd9d3048c867fb652d6db605e804968a
davidlainesv <david.dev@outlook.com>
2023-07-27 12:47:43 -0600

    fix torch for cuda 11.7

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-pip install --force-reinstall torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install --force-reinstall torch torchvision torchaudio
 
 pip install pydub
 pip install datasets
