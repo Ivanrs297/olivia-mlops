85ace85afdad01e927d3d39ffab91b0fc80eaceb
davidlainesv <david.dev@outlook.com>
2023-07-27 14:13:10 -0600

    comment pytorch

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 sudo apt install ffmpeg
 
-pip install --force-reinstall torch torchvision torchaudio
+# pip install --force-reinstall torch torchvision torchaudio
 
 pip install pydub
 pip install datasets
@@ -11,3 +11,12 @@ pip install soundfile
 pip install evaluate
 pip install jiwer
 pip install tensorboardX
+
+
+# cuda-cudart-11.7.99-0
+#   cuda-cupti-11.7.101-0
+#   cuda-libraries-11.7.1-0
+#   cuda-nvrtc-11.7.99-0
+#   cuda-nvtx-11.7.91-0
+#   cuda-runtime-11.7.1-0
+#   cudatoolkit-11.7.0-hd8887f6_11
