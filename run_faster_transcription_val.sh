5e0239229e2cd80c6db335a9e19cee481b7943d8
Ubuntu <ubuntu@ip-172-31-9-81.us-east-2.compute.internal>
2023-08-01 09:22:23 +0000

    script to run validation transcriptions

---
+++ run_faster_transcription_val.sh
@@ -0,0 +1,4 @@
+python faster_transcription.py audio_samples/F-285.wav
+python faster_transcription.py audio_samples/F-429.wav
+python faster_transcription.py audio_samples/F-459.wav
+python faster_transcription.py audio_samples/F-298.wav
