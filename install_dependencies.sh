aaafdeb3f0b1c54d3b969654638dad6516893ad9
davidlainesv <david.dev@outlook.com>
2023-08-07 03:14:53 -0600

    se agregaron dependencias

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -22,3 +22,8 @@ pip install wandb
 #   cuda-nvtx-11.7.91-0
 #   cuda-runtime-11.7.1-0
 #   cudatoolkit-11.7.0-hd8887f6_11
+
+# whisperx examples/F-285.wav --model large-v2 --align_model WAV2VEC2_ASR_LARGE_LV60K_960H --batch_size 4 --compute_type int8 --output_dir diari --language es --diarize --min_speakers 2 --max_speakers 3 --hf_token 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
+
+pip install sentence_transformers
+pip install chromadb==0.3.29
